6c5116603b5ed634afa8299898691b17f914589a
ChrisMcNeice <cmcne016@uottawa.ca>
2020-10-19 03:42:42 -0400

    Added my part. Made sign up files more functional, and added tags for employee or customer

--- app/src/main/java/com/uOttawaSEG/servicenovigrad/SignupActivityCustomer.java
+++ app/src/main/java/com/uOttawaSEG/servicenovigrad/SignupActivityCustomer.java
@@ -15,58 +15,78 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
+public class SignupActivityEmployee extends AppCompatActivity {
 
-public class SignupActivityCustomer extends AppCompatActivity {
-
-    public FirebaseAuth mAuth;
-    public EditText mEmail, mPassword,mUsername;
-
+    public EditText mEmail, mPassword, mUsername;
+    public Button mBtnSignUp, mBtnBack;
+    public boolean canSignIn;
+    private FirebaseAuth mAuth;
+    private UserProfileChangeRequest.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
-        mUsername = findViewById(R.id.Username);
+        mBtnSignUp = findViewById(R.id.btnSignIn);
+        mBtnBack = findViewById(R.id.btnBack);
+        canSignIn = true;
 
         mAuth = FirebaseAuth.getInstance();
 
-        Button mbtnBack, mbtnSignUp;
-        mbtnSignUp = findViewById(R.id.btnSignUp);
-        mbtnBack = findViewById(R.id.btnBack);
-
-        mbtnSignUp.setOnClickListener(new View.OnClickListener() {
+        mBtnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString();
                 String pass = mPassword.getText().toString();
+                final String name = mUsername.getText().toString();
+
                 if (email.isEmpty()){
                     mEmail.setError("Please enter your email");
                     mPassword.requestFocus();
+                    canSignIn = false;
                 }
-                else if (pass.isEmpty()){
-                    mPassword.setError("Please enter your password");
+                if (pass.length()<8 || pass.length() > 32){
+                    mPassword.setError("Please enter a 8-32 character password");
                     mPassword.requestFocus();
+                    canSignIn = false;
                 }
-                else if(email.isEmpty() && pass.isEmpty()){
-                    toastMessage("Fields are empty");
+                for(int i = 0; i<name.length();i++) {
+                    if (!Character.isLetter(name.charAt(i)) || (name.charAt(i) == ' ')) {
+                        mPassword.setError("Please enter an actual name");
+                        mPassword.requestFocus();
+                        canSignIn = false;
+                        break;
+                    }
                 }
-                else if ((!email.isEmpty() && !pass.isEmpty())){
+                if(name.isEmpty()){
+                    mPassword.setError("Please enter an actual name");
+                    mPassword.requestFocus();
+                    canSignIn = false;
+                }
+                if(email.isEmpty() || pass.isEmpty() || name.isEmpty()){
+                    toastMessage("Field(s) are empty");
+                    canSignIn = false;
+                }
+                if(canSignIn) {
                     mAuth.createUserWithEmailAndPassword(email,pass)
-                            .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            .addOnCompleteListener(SignupActivityEmployee.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     Log.d("createUser","Success");
                                     if (task.isSuccessful()) {
                                         Log.d("createUserTask","Success");
                                         toastMessage("New account created!");
-                                        Intent intToHomeActivity = new Intent(MainActivity.this, HomeActivity.class);
+                                        builder = new UserProfileChangeRequest.Builder();
+                                        builder.setDisplayName(name+" Customer");
+                                        Intent intToHomeActivity = new Intent(SignupActivityEmployee.this, WelcomeScreen.class);
                                         startActivity(intToHomeActivity);
                                     }
                                     if (!task.isSuccessful()){
-                                        Log.d("createUserTask","Not Success");
                                         task.getException().printStackTrace();
                                         toastMessage("Sign up unsuccessful.");
                                     }
@@ -76,20 +96,18 @@ public class SignupActivityCustomer extends AppCompatActivity {
                 else {
                     toastMessage("An error has occurred. Please try again!");
                 }
-                // specify as a customer in firebase after creation
-                //redirect to helloCustomer
+            }
+
+            private void toastMessage(String s) {
             }
         });
 
-        mbtnBack.setOnClickListener(new View.OnClickListener() {
+        mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //directs to MainActivity
+                finish();
             }
         });
     }
-    //Customizable toast message maker
-    private void toastMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+
 }
--- app/src/main/java/com/uOttawaSEG/servicenovigrad/SignupActivityEmployee.java
+++ app/src/main/java/com/uOttawaSEG/servicenovigrad/SignupActivityEmployee.java
@@ -1,46 +1,113 @@
 package com.uOttawaSEG.servicenovigrad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignupActivityEmployee extends AppCompatActivity {
+
+    public EditText mEmail, mPassword, mUsername;
+    public Button mBtnSignUp, mBtnBack;
+    public boolean canSignIn;
+    private FirebaseAuth mAuth;
+    private UserProfileChangeRequest.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        EditText mEmail, mPassword,mUsername;
         mEmail = findViewById(R.id.email);
         mPassword = findViewById(R.id.password);
-        mUsername = findViewById(R.id.Username);
+        mBtnSignUp = findViewById(R.id.btnSignIn);
+        mBtnBack = findViewById(R.id.btnBack);
+        canSignIn = true;
 
+        mAuth = FirebaseAuth.getInstance();
 
-        Button mbtnBack, mbtnSignUp;
-        mbtnSignUp = findViewById(R.id.btnSignUp);
-        mbtnBack = findViewById(R.id.btnBack);
-
-        mbtnSignUp.setOnClickListener(new View.OnClickListener() {
+        mBtnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //this is where the .createUserWithEmailAndPassword will be implemented
-                // create entry in firebase
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                final String name = mUsername.getText().toString();
 
-                //redirect to MainActivity after Signup
+                if (email.isEmpty()){
+                    mEmail.setError("Please enter your email");
+                    mPassword.requestFocus();
+                    canSignIn = false;
+                }
+                if (pass.length()<8 || pass.length() > 32){
+                    mPassword.setError("Please enter a 8-32 character password");
+                    mPassword.requestFocus();
+                    canSignIn = false;
+                }
+                for(int i = 0; i<name.length();i++) {
+                    if (!Character.isLetter(name.charAt(i)) || (name.charAt(i) == ' ')) {
+                        mPassword.setError("Please enter an actual name");
+                        mPassword.requestFocus();
+                        canSignIn = false;
+                        break;
+                    }
+                }
+                if(name.isEmpty()){
+                    mPassword.setError("Please enter an actual name");
+                    mPassword.requestFocus();
+                    canSignIn = false;
+                }
+                if(email.isEmpty() || pass.isEmpty() || name.isEmpty()){
+                    toastMessage("Field(s) are empty");
+                    canSignIn = false;
+                }
+                if(canSignIn) {
+                    mAuth.createUserWithEmailAndPassword(email,pass)
+                            .addOnCompleteListener(SignupActivityEmployee.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d("createUser","Success");
+                                    if (task.isSuccessful()) {
+                                        Log.d("createUserTask","Success");
+                                        toastMessage("New account created!");
+                                        builder = new UserProfileChangeRequest.Builder();
+                                        builder.setDisplayName(name+" Employee");
+                                        Intent intToHomeActivity = new Intent(SignupActivityEmployee.this, WelcomeScreen.class);
+                                        startActivity(intToHomeActivity);
+                                    }
+                                    if (!task.isSuccessful()){
+                                        task.getException().printStackTrace();
+                                        toastMessage("Sign up unsuccessful.");
+                                    }
+                                }
+                            });
+                }
+                else {
+                    toastMessage("An error has occurred. Please try again!");
+                }
+            }
+
+            private void toastMessage(String s) {
             }
         });
 
-
-        mbtnBack.setOnClickListener(new View.OnClickListener() {
+        mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //directs to MainActivity
+                finish();
             }
         });
     }
+
 }
